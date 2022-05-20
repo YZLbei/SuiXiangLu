121b600174ae46bba5b0aaeba5c7d3618efd29b9
YZLBei <yuzhenlongbei@outlook.com>
2022-05-20 11:04:52 +0800

    二分查找

---
+++ array/src/main/java/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    /**
+     * 左闭右闭
+     * 一定要注意区间
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int middle = (left+right)/2;
+        while (left<=right){
+            if (nums[middle]==target){
+                return middle;
+            }
+            else if (nums[middle]<target){
+                left = middle+1;
+                middle =(left+right)/2;
+            }
+            else{
+                right = middle-1;
+                middle =(left+right)/2;
+            }
+        }
+        return -1;
+    }
+    
+}
