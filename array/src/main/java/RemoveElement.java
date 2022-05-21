c16296a0f314528d5d9bc775a93216bd5f1567c6
YZLBei <yuzhenlongbei@outlook.com>
2022-05-21 11:07:21 +0800

    27.移除元素（双指针法）

---
+++ array/src/main/java/RemoveElement.java
@@ -0,0 +1,72 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        //result就表示最后的长度
+        int result = 0;
+        //当前遍历到的位置
+        int index = 0;
+        for(int i=0;i<nums.length;i++){
+            for (int j = index; j < nums.length; j++){
+                if (nums[j]!=val){
+                    result++;
+                    nums[i] = nums[j];
+                    index = j+1;
+                    break;
+                }
+            }
+            if (index==nums.length){
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 双指针解法
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement2(int[] nums, int val) {
+        int slow = 0;
+        int fast = 0;
+        while(fast<nums.length){
+            if(nums[slow]!=val){
+                fast++;
+                slow++;
+            }
+            else {
+                if(nums[fast]!=val){
+                    nums[slow] = nums[fast];
+                    nums[fast] = val;
+                    slow++;
+                    fast++;
+                }
+                else{
+                    fast++;
+                }
+            }
+        }
+        return slow;
+    }
+
+
+    /**
+     * 更好的双指针思路
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement3(int[] nums, int val) {
+        int slow = 0;
+        int fast= 0;
+        for (fast = 0; fast < nums.length; fast++) {
+            if (nums[fast]!=val){
+                nums[slow++] =nums[fast];
+            }
+        }
+        return slow;
+    }
+    
+    
+}
