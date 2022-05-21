f1e14f980b5110bf23b8118fc3e7347901eb106a
YZLBei <yuzhenlongbei@outlook.com>
2022-05-21 11:26:47 +0800

    27.移除元素（双指针法）

---
+++ array/src/main/java/test.java
@@ -0,0 +1,15 @@
+public class test {
+    public int search2(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid;
+        }
+        return -1;
+    }
+}
