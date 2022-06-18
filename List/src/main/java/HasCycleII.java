3d056a07232f7b334542f5b6c35d776314e9db0b
YZLBei <yuzhenlongbei@outlook.com>
2022-06-18 11:07:34 +0800

    环形链表II（快慢指针）

---
+++ List/src/main/java/HasCycleII.java
@@ -0,0 +1,26 @@
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/6/18 10:56
+ * @Description:
+ */
+public class HasCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head,slow =head;
+        while (fast!=null&&fast.next!=null){
+            fast =fast.next.next;
+            slow = slow.next;
+            if (slow==fast){
+                break;
+            }
+        }
+        if (fast==null||fast.next==null){
+            return null;
+        }
+        slow = head;
+        while (slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
