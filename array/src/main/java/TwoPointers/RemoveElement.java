006f7afd8bc234a1149bb15941194a7b78e783a9
YZLBei <yuzhenlongbei@outlook.com>
2022-06-25 10:36:35 +0800

    数组快慢指针（移除元素）

---
+++ array/src/main/java/TwoPointers/RemoveElement.java
@@ -0,0 +1,19 @@
+package TwoPointers;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/6/25 10:36
+ * @Description:
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast]!=val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
