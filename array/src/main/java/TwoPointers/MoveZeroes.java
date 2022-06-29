a03681e2f0b26b764f80ac3407c5783ac561ade7
YZLBei <yuzhenlongbei@outlook.com>
2022-06-29 16:13:39 +0800

    数组快慢指针（移动零）

---
+++ array/src/main/java/TwoPointers/MoveZeroes.java
@@ -0,0 +1,22 @@
+package TwoPointers;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/6/25 10:36
+ * @Description: 移动零
+ * 快慢指针法
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast]!=0){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
