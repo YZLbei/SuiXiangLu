21bba5dc20dc65312aa364ddf9869afb38b8034c
YZLBei <yuzhenlongbei@outlook.com>
2022-07-23 22:17:10 +0800

    所有重复子树

---
+++ binary/src/main/java/post/FindDuplicateSubtrees.java
@@ -0,0 +1,32 @@
+package post;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/7/23 21:21
+ * @Description:
+ */
+public class FindDuplicateSubtrees {
+    HashMap<String, Integer>map = new HashMap<>();
+    List<TreeNode> res = new ArrayList<>();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        serialize(root);
+        return res;
+    }
+    public String serialize(TreeNode root){
+        if (root==null){
+            return "#";
+        }
+        String left = serialize(root.left);
+        String right = serialize(root.right);
+        String myself =left+","+right+","+root.val;
+        Integer i = map.getOrDefault(myself, 0);
+        if (i==1){
+            res.add(root);
+        }
+        map.put(myself,i+1);
+        return myself;
+    }
+}
