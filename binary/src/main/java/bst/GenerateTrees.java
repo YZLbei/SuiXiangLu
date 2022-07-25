d7d5fc9946eeff535c674e78e0e04c1468fd5c58
YZLBei <yuzhenlongbei@outlook.com>
2022-07-25 20:23:41 +0800

    不同的二叉搜索树II

--- binary/src/main/java/bst/GenerateTrees.java
+++ binary/src/main/java/bst/GenerateTrees.java
@@ -12,36 +12,29 @@ import java.util.TreeMap;
  */
 public class GenerateTrees {
     public List<TreeNode> generateTrees(int n) {
-        if (n==1){
-            List<TreeNode> res = new ArrayList<>();
-            res.add(new TreeNode(1));
+        return build(1,n);
+    }
+    //构建[low,high]之间的所有二叉树
+    public List<TreeNode> build(int low,int high){
+        List<TreeNode> res = new LinkedList<>();
+        if (low>high){
+            res.add(null);
             return res;
         }
-        //dp[i]表示i个节点的二叉搜索树数量
-        List<TreeNode>[]dp =new LinkedList[n+1];
-        for (int i = 0; i <=n; i++) {
-            dp[i]= new ArrayList<>();
-        }
-        dp[1].add(new TreeNode(1));
-        TreeNode tree1 = new TreeNode(1);
-        tree1.right = new TreeNode(2);
-        TreeNode tree2 = new TreeNode(2);
-        tree2.left = new TreeNode(1);
-        dp[2].add(tree2);
-        dp[2].add(tree1);
-        for (int i = 3; i < n; i++) {
-            //表示树以j为根节点的情况
-            for (int j = 1; j <=i; j++) {
-                TreeNode tree = new TreeNode(j);
-                for (int k = 0; k < dp[j - 1].size(); k++) {
-                    for (int l = 0; l < dp[i - j].size(); l++) {
-                        tree.left = dp[j-1].get(k);
-                        tree.right = dp[i-j].get(l);
-                        dp[i].add(tree);
-                    }
+
+        //以i为根节点
+        for (int i = low; i <=high; i++) {
+            List<TreeNode> left = build(low, i - 1);
+            List<TreeNode> right = build(i + 1, high);
+            for (TreeNode k : left) {
+                for (TreeNode p : right) {
+                    TreeNode tree = new TreeNode(i);
+                    tree.left = k;
+                    tree.right = p;
+                    res.add(tree);
                 }
             }
         }
-        return dp[n];
+        return res;
     }
 }
