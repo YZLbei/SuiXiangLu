47fde8226ea4a07fe4dddde76b6d237d8418655b
YZLBei <yuzhenlongbei@outlook.com>
2022-07-25 12:05:18 +0800

    验证二叉搜索树

--- binary/src/main/java/bst/IsValidBST.java
+++ binary/src/main/java/bst/IsValidBST.java
@@ -6,24 +6,19 @@ package bst;
  * @Description:
  */
 public class IsValidBST {
-    public boolean isValidBST(TreeNode root) {
-        if (root==null){
-            return true;
-        }
-        boolean left = isValidBST(root.left);
-        boolean right = isValidBST(root.right);
-        return cur&&left&&right;
+    boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
     }
-    
-    
-    public int traverse(TreeNode root ,int cur){
-        if (root==null){
-            return 0;
-        }
-        if (root.val<cur){
-            min  = cur;
-        }
-        
-        
+
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null) return true;
+        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+        return isValidBST(root.left, min, root)
+                && isValidBST(root.right, root, max);
     }
 }
