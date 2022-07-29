d39677081df9dea65f7417f4448251ba99d711d4
YZLBei <yuzhenlongbei@outlook.com>
2022-07-29 20:13:44 +0800

    最长子数组（动态规划优化）

--- dp/src/main/java/MaxSubArray.java
+++ dp/src/main/java/MaxSubArray.java
@@ -30,4 +30,23 @@ public class MaxSubArray {
         }
         return result;
     }
+
+
+    public int maxSubArray3(int[] nums) {
+        if (nums.length ==1){
+            return nums[0];
+        }
+        //dp[i][0]表示要dp[i],dp[i][1]表示不要dp[i]
+        //int []dp =new int[nums.length];
+        int dp0 = nums[0];
+        int dp1 = nums[1];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp1 = Integer.max(dp0+nums[i],nums[i]);
+            //dp[i] = Integer.max(dp[i-1]+nums[i],nums[i]);
+            result = Integer.max(result,dp1);
+            dp0 = dp1;
+        }
+        return result;
+    }
 }
