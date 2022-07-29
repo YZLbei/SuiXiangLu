9198f9c621aa06ca88f0772603a5edbb8434880e
YZLBei <yuzhenlongbei@outlook.com>
2022-07-29 20:09:41 +0800

    最长子数组（动态规划）

--- dp/src/main/java/MaxSubArray.java
+++ dp/src/main/java/MaxSubArray.java
@@ -18,14 +18,16 @@ public class MaxSubArray {
     }
 
     public int maxSubArray2(int[] nums) {
-        //
-        int []dp =new int[nums.length][2];
+        //dp[i][0]表示要dp[i],dp[i][1]表示不要dp[i]
+        int []dp =new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             dp[i] = nums[i];
         }
-        int result = 0;
+        int result = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Integer.max(dp[i-1],dp[i-1]+nums[i]);
+            dp[i] = Integer.max(dp[i-1]+nums[i],nums[i]);
+            result = Integer.max(result,dp[i]);
         }
+        return result;
     }
 }
