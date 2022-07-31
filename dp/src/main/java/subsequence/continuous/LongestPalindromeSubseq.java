63b8bc24d8c6196e7f44d4c6f6a7f6c19fbb46a5
YZLBei <yuzhenlongbei@outlook.com>
2022-07-31 09:33:14 +0800

    最长回文子序列（注意遍历方向）

---
+++ dp/src/main/java/subsequence/continuous/LongestPalindromeSubseq.java
@@ -0,0 +1,31 @@
+package subsequence.continuous;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/7/30 11:13
+ * @Description:
+ */
+public class LongestPalindromeSubseq {
+    public int longestPalindromeSubseq(String s) {
+        if (s.length()==1){
+            return 1;
+        }
+        int n = s.length();
+        //dp[i][j]表示[i,j]的最长回文子序列长度
+        int [][]dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n; i >=0; i--) {
+            for (int j = i+1; j < n; j++) {
+                if (s.charAt(i)==s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1]+2;
+                }
+                else {
+                    dp[i][j] = Integer.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
