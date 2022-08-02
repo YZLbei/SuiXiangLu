581044d1771be224f9352ce5d45cabaaed6db7dd
YZLBei <yuzhenlongbei@outlook.com>
2022-08-02 09:14:36 +0800

    完全背包（零钱兑换）

--- dp/src/main/java/backpack/complete/Change.java
+++ dp/src/main/java/backpack/complete/Change.java
@@ -8,5 +8,17 @@ package backpack.complete;
  * 相当于完全背包问题
  */
 public class Change {
-    
+    public int change(int amount, int[] coins) {
+        //dp[i]表示有多少种方式凑出i
+        int []dp = new int[amount+1];
+        
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            //完全背包问题一定是正序的
+            for (int j = coins[i]; j <=amount; j++) {
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
 }
