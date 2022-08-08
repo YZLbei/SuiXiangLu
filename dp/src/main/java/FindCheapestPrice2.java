43e750846ddaadd45e58d8d7db3b4adf5e7c28cf
YZLBei <yuzhenlongbei@outlook.com>
2022-08-08 11:40:54 +0800

    787. K 站中转内最便宜的航班（DP）

---
+++ dp/src/main/java/FindCheapestPrice2.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/8/8 10:29
+ * @Description:动态规划
+ */
+public class FindCheapestPrice2 {
+    //哈希表记录入度
+    // to -> [from, price]
+    HashMap<Integer, List<int[]>> indegree;
+    int src, dst;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        indegree = new HashMap<>();
+        this.src =src;
+        this.dst =dst;
+        for (int[] flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            //判断键值是否存在，不存在则插入
+            indegree.putIfAbsent(to,new LinkedList<>());
+            indegree.get(to).add(new int[]{from,price});
+        }
+        return dp(dst, k);
+    }
+    //从src到i，k步之内的最小的距离
+    public int dp(int i,int k){
+        //base case
+        if (i==src) {
+            return 0;
+        }
+        //步数耗尽
+        if (k==-1){
+            return -1;
+        }
+
+        List<int[]> ints = indegree.get(i);
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j < ints.size(); j++) {
+            int from = ints.get(j)[0];
+            int price = ints.get(j)[1];
+            int dp = dp(from, k - 1);
+            if (dp==-1){
+                continue;
+            }
+            //System.out.println(dp(from,k-1)+price);
+            res = Integer.min(res,dp+price);
+        }
+        return res==Integer.MAX_VALUE?-1:res;
+    }
+}
