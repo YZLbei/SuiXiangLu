68ab9a833441885baedafc420441c3d7ab46b809
YZLBei <yuzhenlongbei@outlook.com>
2022-08-15 09:39:16 +0800

    买股票的最佳时机

---
+++ dp/src/main/java/stock/MaxProfit.java
@@ -0,0 +1,23 @@
+package stock;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/8/15 09:30
+ * @Description:
+ */
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int min = prices[0];
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            if (prices[i]<min){
+                min = prices[i];
+            }
+            if (prices[i]>min){
+                res = Integer.max(res,prices[i]-min);
+            }
+        }
+        return res==Integer.MIN_VALUE?0:res;
+    }
+}
