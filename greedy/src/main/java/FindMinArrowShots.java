847db85386e6dae57e4668ef9ccc2f9c05aeae2e
YZLBei <yuzhenlongbei@outlook.com>
2022-08-18 09:09:25 +0800

    区间问题的总结

--- greedy/src/main/java/FindMinArrowShots.java
+++ greedy/src/main/java/FindMinArrowShots.java
@@ -12,17 +12,24 @@ public class FindMinArrowShots {
     //按照左边界排序
     public int findMinArrowShots(int[][] points) {
         int n = points.length;
-        Arrays.sort(points, Comparator.comparing(o->o[0]));
+        Arrays.sort(points,(a,b)->{
+            if (a[0]==b[0]){
+                return Integer.compare(a[1],b[1]);
+            }
+            else {
+                return Integer.compare(a[0],b[0]);
+            }
+        });
 //        for (int i = 0; i < points.length; i++) {
 //            System.out.println(points[i][0]+"   "+points[i][1]);
 //        }
         int res = 1;
-        long right = points[0][1] ;
+        int right = points[0][1] ;
         //System.out.println(right);
         for (int i = 1; i < n; i++) {
             //有重叠要更新重叠边界，为最小边界
             if (points[i][0]<=right){
-                right = Long.min(right,points[i][1]);
+                right = Integer.min(right,points[i][1]);
             }
             if (points[i][0]>right){
                 res+=1;
