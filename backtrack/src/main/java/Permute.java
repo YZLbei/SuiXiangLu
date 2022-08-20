7aa24f61c13810d51082c45201c755c91c449f81
YZLBei <yuzhenlongbei@outlook.com>
2022-08-20 10:08:40 +0800

    全排列

---
+++ backtrack/src/main/java/Permute.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/8/20 09:48
+ * @Description:全排列
+ * 不含重复数字
+ */
+public class Permute {
+    List<List<Integer>>result = new LinkedList<>();
+    LinkedList<Integer>path = new LinkedList<>();
+    public List<List<Integer>> permute(int[] nums) {
+         backtrack(nums);
+         return result;
+    }
+    public void backtrack(int[] nums){
+        int n = nums.length;
+        if (path.size()==n){
+            result.add(new LinkedList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!path.contains(nums[i])) {
+                path.add(nums[i]);
+                backtrack(nums);
+                path.removeLast();
+            }
+        }
+    }
+    
+}
--- greedy/src/main/java/interval/VideoStitching.java
+++ greedy/src/main/java/interval/VideoStitching.java
@@ -1,3 +1,5 @@
+package interval;
+
 import java.util.Arrays;
 
 /**
