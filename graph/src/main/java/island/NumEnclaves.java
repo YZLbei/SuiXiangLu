bb5e3153adbd389d73862efc181d0a5a4d32d179
YZLBei <yuzhenlongbei@outlook.com>
2022-08-29 11:22:22 +0800

    飞地数量（用visited实现，超时）

--- graph/src/main/java/island/NumEnclaves.java
+++ graph/src/main/java/island/NumEnclaves.java
@@ -9,149 +9,62 @@ public class NumEnclaves {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        visited = new int[m][n];
+        visited = new boolean[m][n];
         int res = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j]==0||(grid[i][j]==1&&visited[i][j]==1)){
+                if (grid[i][j]==0){
                     continue;
                 }
-                else if (grid[i][j]==1&&grid[i][j]==0){
+                else if (grid[i][j]==1){
                     if (!dfs(grid,i,j)){
                         res++;
                     }
                 }
-                else if (grid[i][j]==1&&grid[i][j]==2){
-                    res++;
-                }
+
             }
         }
         return res;
     }
-    //visited为0表示没访问过，1表示访问过可以到达边界，2表示访问过不可以到达边界,3表示访问过不知道是否能到达边界
-    int [][]visited;
+    //visited为false表示没访问过，true表示访问过
+    boolean [][]visited;
     public boolean dfs(int[][] grid,int i ,int j){
         int m = grid.length;
         int n = grid[0].length;
         if(i==0||j==0||i==m-1||j==n-1){
             return true;
         }
-        if (visited[i][j]==1){
-            return true;
-        }
-        else if(visited[i][j]==2){
-            
-        }
-        visited[i][j] =3;
+        visited[i][j] =true;
         //选择
-        if (i>0&&grid[i-1][j]==1){
-            if (visited[i][j]==1){
-                return true;
-            }
-            else if(visited[i][j]==2){
-                
-            }
-            else if (){
-                
-            }
+        if (i>0&&grid[i-1][j]==1&&!visited[i-1][j]){
             boolean a = dfs(grid, i - 1, j);
             if (a){
-                visited[i][j] = 1;
+                visited[i][j] = false;
                 return true;
             }
         }
         if (j>0&&grid[i][j-1]==1&&!visited[i][j-1]){
             boolean b = dfs(grid, i, j-1);
             if (b){
-                visited[i][j] = 1;
+                visited[i][j] = false;
                 return true;
             }
         }
         if (j<n-1&&grid[i][j+1]==1&&!visited[i][j+1]){
             boolean c = dfs(grid, i, j+1);
             if (c){
-                visited[i][j] = 1;
+                visited[i][j] = false;
                 return true;
             }
         }
         if (i<m-1&&grid[i+1][j]==1&&!visited[i+1][j]){
             boolean d = dfs(grid, i+1, j);
             if (d){
-                visited[i][j] = 1;
+                visited[i][j] = false;
                 return true;
             }
         }
-        visited[i][j] = 2;
+        visited[i][j] = false;
         return false;
     }
-
-
-
-
-
-
-
-
-
-//    public int numEnclaves(int[][] grid) {
-//        int m = grid.length;
-//        int n = grid[0].length;
-//        visited = new boolean[m][n];
-//        int res = 0;
-//        for (int i = 0; i < m; i++) {
-//            for (int j = 0; j < n; j++) {
-//                if (grid[i][j]==0){
-//                    continue;
-//                }
-//                else if (grid[i][j]==1){
-//                    if (!dfs(grid,i,j)){
-//                        res++;
-//                    }
-//                }
-//
-//            }
-//        }
-//        return res;
-//    }
-//    //visited为false表示没访问过，true表示访问过
-//    boolean [][]visited;
-//    public boolean dfs(int[][] grid,int i ,int j){
-//        int m = grid.length;
-//        int n = grid[0].length;
-//        if(i==0||j==0||i==m-1||j==n-1){
-//            return true;
-//        }
-//        visited[i][j] =true;
-//        //选择
-//        if (i>0&&grid[i-1][j]==1&&!visited[i-1][j]){
-//            boolean a = dfs(grid, i - 1, j);
-//            if (a){
-//                visited[i][j] = false;
-//                return true;
-//            }
-//        }
-//        if (j>0&&grid[i][j-1]==1&&!visited[i][j-1]){
-//            boolean b = dfs(grid, i, j-1);
-//            if (b){
-//                visited[i][j] = false;
-//                return true;
-//            }
-//        }
-//        if (j<n-1&&grid[i][j+1]==1&&!visited[i][j+1]){
-//            boolean c = dfs(grid, i, j+1);
-//            if (c){
-//                visited[i][j] = false;
-//                return true;
-//            }
-//        }
-//        if (i<m-1&&grid[i+1][j]==1&&!visited[i+1][j]){
-//            boolean d = dfs(grid, i+1, j);
-//            if (d){
-//                visited[i][j] = false;
-//                return true;
-//            }
-//        }
-//        visited[i][j] = false;
-//        return false;
-//    }
 }
