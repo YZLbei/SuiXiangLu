0a0b8901278e4ee7d61f0e99e1d04fa7baad15e0
YZLBei <yuzhenlongbei@outlook.com>
2022-09-07 22:55:40 +0800

    同程笔试

---
+++ exam/src/main/java/tongcheng.java
@@ -0,0 +1,30 @@
+/**
+ * @Auther: YuZhenLong
+ * @Date: 2022/9/7 19:28
+ * @Description:
+ */
+public class tongcheng {
+    public static String longestPrefix (String s) {
+        // write code here
+        int left  =0;
+        int right = s.length()-1;
+        StringBuilder res = new StringBuilder();
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
+        while (left<=s.length()-2){
+            a.append(s.charAt(left));
+            b.insert(0,s.charAt(right));
+            if (a.toString().equals(b.toString())){
+                res = new StringBuilder(a);
+            }
+            left++;
+            right--;
+            //System.out.println(res);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPrefix("ababab"));
+    }
+}
