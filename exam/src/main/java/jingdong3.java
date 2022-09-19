f947e255c8d1acecd2513422121ab30ba5e8fdf0
YZLBei <yuzhenlongbei@outlook.com>
2022-09-19 21:21:37 +0800

    京东笔试

--- exam/src/main/java/jingdong3.java
+++ exam/src/main/java/jingdong3.java
@@ -6,12 +6,12 @@ import java.util.*;
  * @Description:
  */
 public class jingdong3 {
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
         int []num = new int[n];
-        
         for (int i = 0; i < n; i++) {
             num[i] = in.nextInt();
         }
